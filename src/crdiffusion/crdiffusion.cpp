69321336c8b9210d2e45a4be2fc297f9129acda7
Erika Nishio <erika.nishio@astr.tohoku.ac.jp>
2024-09-17 18:23:25 +0900

    single binからn binに変数を増やした

--- src/crdiffusion/crdiffusion.cpp
+++ src/crdiffusion/crdiffusion.cpp
@@ -33,31 +33,24 @@
 //! \fn CRDiffusion::CRDiffusion(MeshBlock *pmb, ParameterInput *pin)
 //! \brief CRDiffusion constructor
 CRDiffusion::CRDiffusion(MeshBlock *pmb, ParameterInput *pin) :
-    pmy_block(pmb), 
-    ecr1(pmb->ncells3, pmb->ncells2, pmb->ncells1),
-    ecr2(pmb->ncells3, pmb->ncells2, pmb->ncells1),
-    source1(pmb->ncells3, pmb->ncells2, pmb->ncells1),
-    source2(pmb->ncells3, pmb->ncells2, pmb->ncells1),
-    coeff1(NCOEFF, pmb->ncells3, pmb->ncells2, pmb->ncells1),
-    coeff2(NCOEFF, pmb->ncells3, pmb->ncells2, pmb->ncells1),
+    pmy_block(pmb), ecr(NECR,pmb->ncells3, pmb->ncells2, pmb->ncells1),
+    source(NECR,pmb->ncells3, pmb->ncells2, pmb->ncells1),
+    coeff(NECR,NCOEFF, pmb->ncells3, pmb->ncells2, pmb->ncells1),
     coarse_ecr(pmb->ncc3, pmb->ncc2, pmb->ncc1,
               (pmb->pmy_mesh->multilevel ? AthenaArray<Real>::DataStatus::allocated :
                AthenaArray<Real>::DataStatus::empty)),
     empty_flux{AthenaArray<Real>(), AthenaArray<Real>(), AthenaArray<Real>()},
     output_defect(false), crbvar(pmb, &ecr, &coarse_ecr, empty_flux, false),
     refinement_idx_(), 
-    Dpara1_(), Dperp1_(), Lambda1_(),
-    Dpara2_(), Dperp2_(), Lambda2_() {
-  Dpara1_ = pin->GetReal("crdiffusion", "Dpara1");
-  Dperp1_ = pin->GetReal("crdiffusion", "Dperp1");
-  Lambda1_ = pin->GetReal("crdiffusion", "Lambda1");
-  Dpara2_ = pin->GetReal("crdiffusion", "Dpara2");
-  Dperp2_ = pin->GetReal("crdiffusion", "Dperp2");
-  Lambda2_ = pin->GetReal("crdiffusion", "Lambda2");
+    Dpara(NECR), Dperp(NECR), Lambda(NECR),NECRbin_() {
+  //Dpara_ = pin->GetReal("crdiffusion", "Dpara");
+  //Dperp_ = pin->GetReal("crdiffusion", "Dperp");
+  //Lambda_ = pin->GetReal("crdiffusion", "Lambda");
+  NECRbin_ = pin->GetReal("crdiffusion", "NECRbin");//the number of cosmic ray bin
 
   output_defect = pin->GetOrAddBoolean("crdiffusion", "output_defect", false);
   if (output_defect)
-    def.NewAthenaArray(pmb->ncells3, pmb->ncells2, pmb->ncells1);
+    def.NewAthenaArray(NECR,pmb->ncells3, pmb->ncells2, pmb->ncells1);
 
   pmb->RegisterMeshBlockData(ecr);
   // "Enroll" in S/AMR by adding to vector of tuples of pointers in MeshRefinement class
@@ -93,43 +86,38 @@ void CRDiffusion::CalculateCoefficients(const AthenaArray<Real> &w,
     jl -= NGHOST, ju += NGHOST;
   if (pmy_block->pmy_mesh->f3)
     kl -= NGHOST, ku += NGHOST;
-  Real Dpara1 = Dpara1_, Dperp1 = Dperp1_, Lambda1 = Lambda1_;
-  Real Dpara2 = Dpara2_, Dperp2 = Dperp2_, Lambda2 = Lambda2_;
+  //Real Dpara = Dpara_, Dperp = Dperp_, Lambda = Lambda_;
 
   if (MAGNETIC_FIELDS_ENABLED) {
-    for (int k = kl; k <= ku; ++k) {
-      for (int j = jl; j <= ju; ++j) {
-        for (int i = il; i <= iu; ++i) {
-          const Real &bx = bcc(IB1,k,j,i);
-          const Real &by = bcc(IB2,k,j,i);
-          const Real &bz = bcc(IB3,k,j,i);
-          Real ba = std::sqrt(SQR(bx) + SQR(by) + SQR(bz) + TINY_NUMBER);
-          Real nx = bx / ba, ny = by / ba, nz = bz / ba;
-          coeff1(DXX,k,j,i) = Dperp1 + (Dpara1 - Dperp1) * nx * nx;
-          coeff1(DXY,k,j,i) =         (Dpara1 - Dperp1) * nx * ny;
-          coeff1(DXZ,k,j,i) =         (Dpara1 - Dperp1) * nx * nz;
-          coeff1(DYY,k,j,i) = Dperp1 + (Dpara1 - Dperp1) * ny * ny;
-          coeff1(DYZ,k,j,i) =         (Dpara1 - Dperp1) * ny * nz;
-          coeff1(DZZ,k,j,i) = Dperp1 + (Dpara1 - Dperp1) * nz * nz;
-          coeff1(NLAMBDA, k,j,i) = Lambda1 * w(IDN,k,j,i);
-
-          coeff2(DXX,k,j,i) = Dperp2 + (Dpara2 - Dperp2) * nx * nx;
-          coeff2(DXY,k,j,i) =         (Dpara2 - Dperp2) * nx * ny;
-          coeff2(DXZ,k,j,i) =         (Dpara2 - Dperp2) * nx * nz;
-          coeff2(DYY,k,j,i) = Dperp2 + (Dpara2 - Dperp2) * ny * ny;
-          coeff2(DYZ,k,j,i) =         (Dpara2 - Dperp2) * ny * nz;
-          coeff2(DZZ,k,j,i) = Dperp2 + (Dpara2 - Dperp2) * nz * nz;
-          coeff2(NLAMBDA, k,j,i) = Lambda2 * w(IDN,k,j,i);
+    for (int n=0; n <= NECRbin; ++n){
+      for (int k = kl; k <= ku; ++k) {
+        for (int j = jl; j <= ju; ++j) {
+          for (int i = il; i <= iu; ++i) {
+            const Real &bx = bcc(IB1,k,j,i);
+            const Real &by = bcc(IB2,k,j,i);
+            const Real &bz = bcc(IB3,k,j,i);
+            Real ba = std::sqrt(SQR(bx) + SQR(by) + SQR(bz) + TINY_NUMBER);
+            Real nx = bx / ba, ny = by / ba, nz = bz / ba;
+            coeff(n,DXX,k,j,i) = Dperp(n) + (Dpara(n)  - Dperp(n) ) * nx * nx;
+            coeff(n,DXY,k,j,i) =         (Dpara(n)  - Dperp(n) ) * nx * ny;
+            coeff(n,DXZ,k,j,i) =         (Dpara(n)  - Dperp(n) ) * nx * nz;
+            coeff(n,DYY,k,j,i) = Dperp(n)  + (Dpara(n)  - Dperp(n) ) * ny * ny;
+            coeff(n,DYZ,k,j,i) =         (Dpara(n)  - Dperp(n) ) * ny * nz;
+            coeff(n,DZZ,k,j,i) = Dperp(n)  + (Dpara(n)  - Dperp(n)) * nz * nz;
+            coeff(n,NLAMBDA, k,j,i) = Lambda(n)  * w(IDN,k,j,i);
+          }
         }
       }
     }
   } else {
-    for (int k = kl; k <= ku; ++k) {
-      for (int j = jl; j <= ju; ++j) {
-        for (int i = il; i <= iu; ++i) {
-          coeff(DXX,k,j,i) = coeff(DXY,k,j,i) = coeff(DXZ,k,j,i) = coeff(DYY,k,j,i)
-                           = coeff(DYZ,k,j,i) = coeff(DZZ,k,j,i) = Dpara;
-          coeff(NLAMBDA, k,j,i) = Lambda * w(IDN,k,j,i);
+    for (int n=0; n <= NECRbin; ++n){
+      for (int k = kl; k <= ku; ++k) {
+        for (int j = jl; j <= ju; ++j) {
+          for (int i = il; i <= iu; ++i) {
+            coeff(n,DXX,k,j,i) = coeff(n,DXY,k,j,i) = coeff(n,DXZ,k,j,i) = coeff(n,DYY,k,j,i)
+                            = coeff(n,DYZ,k,j,i) = coeff(n,DZZ,k,j,i) = Dpara(n);
+            coeff(n,NLAMBDA, k,j,i) = Lambda(n) * w(IDN,k,j,i);
+          }
         }
       }
     }
